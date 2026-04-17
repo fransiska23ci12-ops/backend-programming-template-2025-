5c5063850b2791b1573b5b98274cf425bb2beb67
Fransiska-ytjf <fransiska23ci12@gmail.com>
2026-04-17 16:49:02 +0700

    Update books-route.js

--- src/api/components/books/books-route.js
+++ src/api/components/books/books-route.js
@@ -7,17 +7,13 @@ const route = express.Router();
 module.exports = (app) => {
   app.use('/books', route);
 
-  // Get list of books
+  
   route.get('/', booksController.getBooks);
 
-  // Create a new book
+
   route.post('/', booksController.createBook);
   
   route.get('/:id', booksController.getBook);
   
-  // TODO: Get a book by id
 
-  // TODO: Update a book by id
-
-  // TODO: Delete a book by id
 };
