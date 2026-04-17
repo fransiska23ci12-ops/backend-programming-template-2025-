2f15c918030c85e49abdf4fca521d4bf88f1d4d6
Fransiska-ytjf <fransiska23ci12@gmail.com>
2026-04-17 17:04:15 +0700

    Update gacha-route.js

--- src/api/components/gacha/gacha-route.js
+++ src/api/components/gacha/gacha-route.js
@@ -2,22 +2,25 @@ const express = require('express');
 
 module.exports = (app) => {
   const route = express.Router();
+  app.use('/gacha', route);
 
-  route.get('/', (req, res) => {
-    const items = [
-      { name: "Karakter SSR", rarity: "SSR" },
-      { name: "Karakter SR", rarity: "SR" },
-      { name: "Karakter R", rarity: "R" }
-    ];
+  route.get('/roll', (req, res) => {
 
-    const randomIndex = Math.floor(Math.random() * items.length);
-    const result = items[randomIndex];
+    const pool = [];
+
+   
+    pool.push(...Array(1).fill({ name: 'Emas 10 gram', rarity: 'SSR' }));
+    pool.push(...Array(5).fill({ name: 'Smartphone X', rarity: 'SSR' }));
+    pool.push(...Array(10).fill({ name: 'Smartwatch Y', rarity: 'SR' }));
+    pool.push(...Array(100).fill({ name: 'Voucher Rp100.000', rarity: 'R' }));
+    pool.push(...Array(500).fill({ name: 'Pulsa Rp50.000', rarity: 'R' }));
+
+  
+    const random = pool[Math.floor(Math.random() * pool.length)];
 
     res.json({
-      message: "Gacha result",
-      data: result
+      message: 'Gacha result',
+      data: random,
     });
   });
-
-  app.use('/gacha', route);
 };
